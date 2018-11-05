f771f8318e75c380ee16e30ddf760eaad115fd8e
EjonLee <a1192872253@163.com>
2018-11-05 09:22:43 +0800

    Create 1.php

---
+++ 1.php
@@ -0,0 +1,14 @@
+<?php
+require("db_comm.php");
+$sql = "select * from student";
+$result = mysqli_query($conn,$sql);
+$n = mysqli_num_rows($result);
+for($i = 0;$i<$n;$i++)
+{
+    $row = mysqli_fetch_array($result);
+    echo "编号：".$row["studentID"]."<br/>";
+    echo "名称：".$row["studentName"]."<br/>";
+    echo "年龄：".$row["age"]."<br/>";
+}
+echo "总人数：{$n}<br/>";
+?>
